d9e47ee5a8b40712b39aaf1698abe494613fde79
Gbadebo Oluwarimi <oluwarimigbadebo@gmail.com>
2025-04-30 11:26:31 +0100

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen overflow-hidden">
-      <div className="absolute inset-0 bg-gradient-to-b from-[#000000] via-[#0d0d0e] to-[#062a0a] opacity-90 -z-10" />
+    <main className="relative min-h-screen overflow-hidden bg-[url('/bgbg.webp')] bg-cover bg-center">
+      <div className="absolute inset-0 bg-gradient-to-b from-[#000000] via-[#0d0d0e] to-[#062a0a] opacity-90 -z-10 " />
       <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHZpZXdCb3g9IjAgMCA2MCA2MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxnIGZpbGw9IiNmZmYiIGZpbGwtb3BhY2l0eT0iMC4xIj48cGF0aCBkPSJNMzYgMzRjMC0yLjIwOS0xLjc5MS00LTQtNHMtNCAxLjc5MS00IDQgMS43OTEgNCA0IDQgNC0xLjc5MSA0LTR6bTAtOGMwLTIuMjA5LTEuNzkxLTQtNC00cy00IDEuNzkxLTQgNCAxLjc5MSA0IDQgNCA0LTEuNzkxIDQtNHptLTE2IDhjMC0yLjIwOS0xLjc5MS00LTQtNHMtNCAxLjc5MS00IDQgMS43OTEgNCA0IDQgNC0xLjc5MSA0LTR6bTE2IDE2YzAtMi4yMDktMS43OTEtNC00LTRzLTQgMS43OTEtNCA0IDEuNzkxIDQgNCA0IDQtMS43OTEgNC00eiIvPjwvZz48L2c+PC9zdmc+')] opacity-5 -z-10" />
 
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
